3c87d6c2e7f890d1937cbd754a72038e3220f1d5
lorenzo.riches <lorenzo.riches@wfp.org>
2018-05-04 16:46:34 +0200

    out center

--- OSM_download.py
+++ OSM_download.py
@@ -3,7 +3,7 @@
 import click
 from shapely.geometry import Polygon, mapping, Point
 from osmnx import bbox_from_point, core
-
+import numpy as np
 
 class OSM_downloader:
     """
@@ -22,15 +22,20 @@ class OSM_downloader:
         query = ('[out:json][timeout:25];'
                  '('
                  'node["amenity"="{amenity:}"]({south:.8f},{west:.8f},{north:.8f},{east:.8f});'
-                 'relation["amenity"="{amenity:}"]({south:.8f},{west:.8f},{north:.8f},{east:.8f});'
-                 ');(._;>;);out;').format(amenity=amenity, north=self.north, south=self.south, east=self.east, west=self.west)
+                 'way["amenity"="{amenity:}"]({south:.8f},{west:.8f},{north:.8f},{east:.8f});'
+                 ');(._;>;);out center;').format(amenity=amenity, north=self.north, south=self.south, east=self.east, west=self.west)
 
         response = core.overpass_request(data={'data': query}, timeout=600, error_pause_duration=None)
-
+        print(response)
         l = []
         for result in response['elements']:
-            if result['type'] == 'node': l.append(print(result['lat'], result['lon']))
+            if result['type'] == 'node': l.append((result['lat'], result['lon']))
 
+            # elif result['type'] == 'way':
+            #     nodes = [l[id] for id in result['nodes']]
+            #     x_l, y_l = [], [] 
+            #     [(x_l.append(x[0]), y_l.append(x[1])) for x in nodes]
+            #     print(np.mean(x_l), np.mean(y_l))
         return l
 
 
